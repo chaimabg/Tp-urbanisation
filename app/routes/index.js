b58960bcf81c44688150d0042b8ac0dffb743c8e
chaimaBenGhanem <bghanem.chaima@gmail.com>
2022-12-06 16:52:05 +0000

    add raml file and api project

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
-
+days = ['Monday','Thuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+months=['january','february','march','april','may','june','july','august','september'
+,'october','november','december'];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/dayOfWeek/:id', function(req, res, next) {
+  const id = req.params.id;
+  
+
+   if(id>0 && id<=7)
+    {let data = `<?xml version="1.0" encoding="UTF-8"?>`;
+      data += `<day>`;
+      data += `
+       <value>${days[id-1]}</value> 
+    `;
+    data += `</day>`;
+    res.header("Content-Type", "application/xml");
+    res.status(200).send(data);
+  }else{
+    res.status(400).send({error : 'provide the right number !'})
+  }
+ 
+    
+});
+router.get('/monthOfYear/:id', function(req, res, next) {
+  const id = req.params.id;
+  
+
+   if(id>0 && id<=12)
+    { let data = `<?xml version="1.0" encoding="UTF-8"?>`;
+      data += `<month>`;
+      data += `
+       <value>${months[id-1]}</value>
+       
+    `;
+    data += `</month>`;
+    res.header("Content-Type", "application/xml");
+    res.status(200).send(data);
+  }else{
+      res.status(400).send({error : 'provide the right number !'})
+    }
+ 
+    
 });
 
 module.exports = router;
